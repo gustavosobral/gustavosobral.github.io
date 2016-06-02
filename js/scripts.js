950c227c6e820829cf581f81fe2d05cabe335acc
Gustavo Sobral <ghsobral@gmail.com>
2016-06-01 23:18:50 -0300

    Add smooth page scroll effect
    Using jQuery animate function

--- js/scripts.js
+++ js/scripts.js
@@ -1,4 +1,13 @@
 (function($){
+
+  $('a.page-scroll').bind('click', function(event) {
+    var $element = $(this);
+    $('html, body').stop().animate({
+        scrollTop: ($($element.attr('href')).offset().top)
+    }, 1450, 'easeInOutExpo');
+    event.preventDefault();
+  });
+
   $(function(){
 
     $('.button-collapse').sideNav();
